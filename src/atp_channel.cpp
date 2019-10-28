d406363bfe06693515ca1bfe6b65406dad391a60
wangpengdc <wangpeng19960620@163.com>
2019-10-28 17:56:35 +0800

    fixed format error

--- src/atp_channel.cpp
+++ src/atp_channel.cpp
@@ -9,105 +9,105 @@ static_assert(ATP_READ_EVENT == EV_READ);
 static_assert(ATP_WRITE_EVENT == EV_WRITE);
 
 Channel::Channel(EventLoop* event_loop, int fd, bool readable, bool writable) {
-	event_loop_ = event_loop;
-	events_ = (readable ? ATP_READ_EVENT : 0) | (writable ? ATP_WRITE_EVENT : 0);
+    event_loop_ = event_loop;
+    events_ = (readable ? ATP_READ_EVENT : 0) | (writable ? ATP_WRITE_EVENT : 0);
 
-	fd_ = fd;
-	assert(fd > 0);
+    fd_ = fd;
+    assert(fd > 0);
 
-	event_ = new(std::nothrow) event;
-	assert(event_);
+    event_ = new(std::nothrow) event;
+    assert(event_);
 
-	memset(event_, 0, sizeof(*event_));
+    memset(event_, 0, sizeof(*event_));
 }
 
 Channel::~Channel() {
-	assert(event_ == NULL);
+    assert(event_ == NULL);
 }
 
 void Channel::attachToEventLoop() {
-	/* The channel is none events, not attach to event loop */
-	if (isNone()) {
-		return;
-	}
+    /* The channel is none events, not attach to event loop */
+    if (isNone()) {
+        return;
+    }
 
-	/* The channel if already attached, we detach from event loop */
-	if (attached_) {
-		detachFromEventLoop();
-	}
+    /* The channel if already attached, we detach from event loop */
+    if (attached_) {
+        detachFromEventLoop();
+    }
 
-	/* Add event_ default is persist event */
-	event_assign(event_, event_loop_->getEventBase(), fd_, 
-		events_ | EV_PERSIST, &Channel::eventHandle, this);
+    /* Add event_ default is persist event */
+    event_assign(event_, event_loop_->getEventBase(), fd_, 
+        events_ | EV_PERSIST, &Channel::eventHandle, this);
 
-	if (!event_add(event_, NULL)) {
-		attached_ = true;
-	}
+    if (!event_add(event_, NULL)) {
+        attached_ = true;
+    }
 }
 
 void Channel::detachFromEventLoop() {
-	/* Delete event_ from the libevent event_base */
-	if (attached_ && event_del(event_) == 0) {
-		attached_ = false;
-	}
+    /* Delete event_ from the libevent event_base */
+    if (attached_ && event_del(event_) == 0) {
+        attached_ = false;
+    }
 }
 
 void Channel::updateEvents() {
-	assert(event_loop_->threadContext());
+    assert(event_loop_->threadContext());
 
-	/* Update the events to none/read/write */
-	if (isNone()) {
-		detachFromEventLoop();
-	} else {
-		attachToEventLoop();
-	}
+    /* Update the events to none/read/write */
+    if (isNone()) {
+        detachFromEventLoop();
+    } else {
+        attachToEventLoop();
+    }
 }
 
 void Channel::close() {
-	if (event_) {
-		detachFromEventLoop();
-		delete event_;
-		event_ = NULL;
-	}
+    if (event_) {
+        detachFromEventLoop();
+        delete event_;
+        event_ = NULL;
+    }
 }
 
 void Channel::enableEvents(bool readable, bool writable) {
-	int orig_events = events_;
+    int orig_events = events_;
 
-	if (readable) {
-		events_ |= ATP_READ_EVENT;
-	}
+    if (readable) {
+        events_ |= ATP_READ_EVENT;
+    }
 
-	if (writable) {
-		events_ |= ATP_WRITE_EVENT;
-	}
+    if (writable) {
+        events_ |= ATP_WRITE_EVENT;
+    }
 
-	if (events_ != orig_events) {
-		updateEvents();
-	}
+    if (events_ != orig_events) {
+        updateEvents();
+    }
 }
 
 void Channel::disableEvents(bool readable, bool writable) {
-	int orig_events = events_;
+    int orig_events = events_;
 
-	if (readable) {
-		events_ &= (~ATP_READ_EVENT);
-	}
+    if (readable) {
+        events_ &= (~ATP_READ_EVENT);
+    }
 
-	if (writable) {
-		events_ &= (~ATP_WRITE_EVENT);
-	}
+    if (writable) {
+        events_ &= (~ATP_WRITE_EVENT);
+    }
 
-	if (events_ != orig_events) {
-		updateEvents();
-	}
+    if (events_ != orig_events) {
+        updateEvents();
+    }
 }
 
 void Channel::disableAllEvents() {
-	if (events_ != ATP_NONE_EVENT) {
-		events_ = ATP_NONE_EVENT;
-		updateEvents();
-	}
+    if (events_ != ATP_NONE_EVENT) {
+        events_ = ATP_NONE_EVENT;
+        updateEvents();
+    }
 }
 
 int Channel::getInternalFd() const {
@@ -115,33 +115,33 @@ int Channel::getInternalFd() const {
 }
 
 std::string Channel::eventsToString() const {
-	std::string events_stringify;
-	if (events_ & ATP_READ_EVENT) {
-		events_stringify += "ATP_READ_EVENT";
-	}
+    std::string events_stringify;
+    if (events_ & ATP_READ_EVENT) {
+        events_stringify += "ATP_READ_EVENT";
+    }
 
-	if (events_ & ATP_WRITE_EVENT) {
-		events_stringify += "|ATP_WRITE_EVENT";
-	}
+    if (events_ & ATP_WRITE_EVENT) {
+        events_stringify += "|ATP_WRITE_EVENT";
+    }
 
-	return events_stringify;
+    return events_stringify;
 }
 
 void Channel::eventHandle(int fd, short which) {
-	assert(fd == fd_);
+    assert(fd == fd_);
 
-	if ((which & ATP_READ_EVENT) && read_cb_ ) {
-		read_cb_();
-	}
+    if ((which & ATP_READ_EVENT) && read_cb_) {
+        read_cb_();
+    }
 
-	if ((which & ATP_WRITE_EVENT) && write_cb_) {
-		write_cb_();
-	}
+    if ((which & ATP_WRITE_EVENT) && write_cb_) {
+        write_cb_();
+    }
 }
 
 void Channel::eventHandle(int fd, short which, void* args) {
-	Channel* owner = static_cast<Channel*>(args);
-	owner->eventHandle(fd, which);
+    Channel* owner = static_cast<Channel*>(args);
+    owner->eventHandle(fd, which);
 }
 
 }/*end namespace atp*/
--- src/atp_channel.h
+++ src/atp_channel.h
@@ -13,64 +13,64 @@ namespace atp {
 
 class Channel {
 public:
-	using EventCallbackPtr = std::function<void()>;
+    using EventCallbackPtr = std::function<void()>;
 	
 public:
-	Channel(EventLoop* event_loop, int fd, bool readable, bool writable);
-	~Channel();
+    Channel(EventLoop* event_loop, int fd, bool readable, bool writable);
+    ~Channel();
 
 public:
-	void attachToEventLoop();
-	void detachFromEventLoop();
-	void updateEvents();
-	void close();
+    void attachToEventLoop();
+    void detachFromEventLoop();
+    void updateEvents();
+    void close();
 
 public:
-	void enableEvents(bool readable, bool writable);
-	void disableEvents(bool readable, bool writable);
-	void disableAllEvents();
-	int getInternalFd() const;
-	std::string eventsToString() const;
+    void enableEvents(bool readable, bool writable);
+    void disableEvents(bool readable, bool writable);
+    void disableAllEvents();
+    int getInternalFd() const;
+    std::string eventsToString() const;
 
 public:
-	/* Inline functions */
-	void setReadCallback(const EventCallbackPtr& cb) {
-		read_cb_ = cb; 
-	}
-	
-	void setWriteCallback(const EventCallbackPtr& cb) {
-		write_cb_ = cb;
-	}
+    /* Inline functions */
+    void setReadCallback(const EventCallbackPtr& cb) {
+        read_cb_ = cb; 
+    }
 
-	bool isAttached() const {
-		return attached_;
-	}
+    void setWriteCallback(const EventCallbackPtr& cb) {
+        write_cb_ = cb;
+    }
 
-	bool isNone() const {
-		return (events_ == ATP_NONE_EVENT);
-	}
+    bool isAttached() const {
+        return attached_;
+    }
 
-	bool isReadable() const {
-		return (events_ & ATP_READ_EVENT) != 0;
-	}
+    bool isNone() const {
+        return (events_ == ATP_NONE_EVENT);
+    }
 
-	bool isWritable() const {
-		return (events_ & ATP_WRITE_EVENT) != 0;
-	}
+    bool isReadable() const {
+        return (events_ & ATP_READ_EVENT) != 0;
+    }
+
+    bool isWritable() const {
+        return (events_ & ATP_WRITE_EVENT) != 0;
+    }
 
 private:
-	void eventHandle(int fd, short which);
-	static void eventHandle(int fd, short which, void* args);
+    void eventHandle(int fd, short which);
+    static void eventHandle(int fd, short which, void* args);
 
 private:
-	EventLoop* event_loop_;			/* IO event loop */
-	struct event* event_;			/* The libevent event */
-	int fd_;						/* The event file description */
-	int events_;					/* The event is read or write or both */
-	bool attached_;					/* The event is weather or not add to libevent event_base */
+    EventLoop* event_loop_;			/* IO event loop */
+    struct event* event_;			/* The libevent event */
+    int fd_;						/* The event file description */
+    int events_;					/* The event is read or write or both */
+    bool attached_;					/* The event is weather or not add to libevent event_base */
 
-	EventCallbackPtr read_cb_;		/* Libevent event read callback function */
-	EventCallbackPtr write_cb_;		/* Libevent event write callback function */
+    EventCallbackPtr read_cb_;		/* Libevent event read callback function */
+    EventCallbackPtr write_cb_;		/* Libevent event write callback function */
 };
 
 }/*end namespace atp*/
